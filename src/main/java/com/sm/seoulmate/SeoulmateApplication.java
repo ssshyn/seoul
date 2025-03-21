209c9d717eb354d86135d8c0d3e45a5ee172e12f
ssshyn <ssshyn.dev@gmail.com>
2025-03-21 22:54:04 +0900

    first commit

---
+++ src/main/java/com/sm/seoulmate/SeoulmateApplication.java
@@ -0,0 +1,13 @@
+package com.sm.seoulmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulmateApplication.class, args);
+	}
+
+}
