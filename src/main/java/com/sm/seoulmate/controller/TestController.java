a44b4ef835d42bf19a79fea05a59774652b2e307
ssshyn <ssshyn.dev@gmail.com>
2025-03-22 17:38:40 +0900

    ci/cd update test

--- src/main/java/com/sm/seoulmate/controller/TestController.java
+++ src/main/java/com/sm/seoulmate/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.sm.seoulmate.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    @Operation(summary = "테스트입니다.", description = "ci/cd swagger Test")
     @GetMapping
     public String test() {
         return "hi";
