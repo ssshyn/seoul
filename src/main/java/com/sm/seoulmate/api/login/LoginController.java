974bfb295742b3be137b2f1799d34ce96c47e20a
ssshyn <ssshyn.dev@gmail.com>
2025-04-12 18:50:43 +0900

    로그인 설정 테스트 3-2

--- src/main/java/com/sm/seoulmate/api/login/LoginController.java
+++ src/main/java/com/sm/seoulmate/api/login/LoginController.java
@@ -4,15 +4,17 @@ import com.google.api.client.auth.oauth2.RefreshTokenRequest;
 import com.sm.seoulmate.api.login.request.LoginRequest;
 import com.sm.seoulmate.domain.login.service.JwtUtil;
 import com.sm.seoulmate.domain.login.service.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class LoginController {
@@ -20,6 +22,10 @@ public class LoginController {
     private final JwtUtil jwtUtil;
     private final LoginService loginService;
 
+    @Operation(
+            summary = "로그인",
+            security = @SecurityRequirement(name = "") // 빈 security 설정
+    )
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
         String jwt = loginService.processLogin(request.getLoginType(), request.getToken());
