55b94ac937f6c25308c5ba0adad3c7f5ff71bf48
ssshyn <ssshyn.dev@gmail.com>
2025-05-09 23:12:00 +0900

    안드로이드 html

---
+++ src/main/java/com/sm/seoulmate/domain/common/RedirectController.java
@@ -0,0 +1,12 @@
+package com.sm.seoulmate.domain.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+    @GetMapping("/android")
+    public String getAndroidList() {
+        return "redirect:/android.html";
+    }
+}
